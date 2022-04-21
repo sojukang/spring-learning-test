cc0b15011ff8b9a64debaad151de23cea786908a
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-04-22 02:06:02 +0900

    feat: Spring core 학습 테스트 완료

--- src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
+++ src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
@@ -1,7 +1,15 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class StationConstructorService {
-    private StationRepository stationRepository;
+
+    private final StationRepository stationRepository;
+
+    public StationConstructorService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
--- src/main/java/nextstep/helloworld/core/di/StationFieldService.java
+++ src/main/java/nextstep/helloworld/core/di/StationFieldService.java
@@ -1,6 +1,11 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class StationFieldService {
+    @Autowired
     private StationRepository stationRepository;
 
     public String sayHi() {
